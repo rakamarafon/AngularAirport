3b5fa7dae56dee956e165ef2ffe3451678e29656
Maksym Akulov <maksakulov@hotmail.com>
2018-07-25 11:16:05 +0300

    added crew component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { PilotComponent } from './pilot/pilot.component';
+import { CrewComponent } from './crew/crew.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    PilotComponent
+    PilotComponent,
+    CrewComponent
   ],
   imports: [
     BrowserModule,
