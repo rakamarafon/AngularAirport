ad85cb541995e37129e99442442dad65e3cf93ee
Maksym Akulov <maksakulov@hotmail.com>
2018-07-25 11:16:33 +0300

    added stewardesses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { PilotComponent } from './pilot/pilot.component';
 import { CrewComponent } from './crew/crew.component';
+import { StewardessesComponent } from './stewardesses/stewardesses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     PilotComponent,
-    CrewComponent
+    CrewComponent,
+    StewardessesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stewardesses/stewardesses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stewardesses',
+  templateUrl: './stewardesses.component.html',
+  styleUrls: ['./stewardesses.component.css']
+})
+export class StewardessesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
